419fff3fabd91d06ce565ec77cdce5bb20930422
William Navarre <git@navarre.xyz>
2024-04-04 01:25:09 -0400

    Bug fixes.

--- script/go.py
+++ script/go.py
@@ -6,6 +6,7 @@ import process_task
 t = my_task.my_task_project.grab_task()
 if t is None: raise ValueError(t)
 try:
+    print("SELECTED THIS TASK: " + repr(t))
     process_task.process_meeting(t)
 except:
     my_task.my_task_project.abandon_task(t)
--- script/process_task.py
+++ script/process_task.py
@@ -1,6 +1,8 @@
 from internetarchive import upload
+import subprocess
 import string
 import datetime
+import os
 
 def new_md():
     return {
@@ -13,17 +15,18 @@ def md(meeting):
     out = new_md()
     out["date"] = meeting["DATE"]
     datetime.datetime.strptime(out["date"], "%Y-%m-%d") # Errors out if format is bad.
+    return out
 
 EXT_CHARS = string.ascii_lowercase + "0123456789"
 
-def identifier(md, meeting):
+def identifier(metadata, meeting):
     slug = meeting["SLUG"].replace(" ", "-").replace("_", "-")
     for e in slug.lower():
         if e == "-" or e in EXT_CHARS:
             pass
         else:
             raise ValueError(e)
-    return "medford_" + meeting["SLUG"] + "_" + md["date"]
+    return "medford_" + slug + "_" + metadata["date"]
 
 def extension(st):
     last_dot = st.rfind(".")
@@ -36,18 +39,24 @@ def extension(st):
     return ext
 
 def process_meeting(meeting):
+    os.chdir(os.getenv("HOME"))
     src_url = meeting["SRC"]
     video_tmpfile = "videofile." + extension(src_url)
     audio_tmpfile = "audiofile.mp3"
     my_md = md(meeting)
-    my_id = identifier(md, meeting)
+    my_id = identifier(my_md, meeting)
     meeting["URL"] = "https://archive.org/download/{}/{}".format(my_id, audio_tmpfile)
-    result = subprocess.run(["wget", src_url, "-O", audio_tmpfile])
-    if result: raise AssertionError("wget")
+    args = ["curl", "-o", video_tmpfile, src_url]
+    result = subprocess.run(args)
+    if result.returncode:
+        print(" ".join(args))
+        print("CURL FAILED!!!!!!!!!\n\n\n\n")
+        raise AssertionError("curl")
     result = subprocess.run(["ffmpeg",
                              "-i", video_tmpfile,
                              "-vn",
                              "-acodec", "copy",
                              audio_tmpfile])
-    if result: raise AssertionError("ffmpeg")
+    if result.returncode: raise AssertionError("ffmpeg")
     upload(my_id, { audio_tmpfile : audio_tmpfile }, metadata=my_md)
+
