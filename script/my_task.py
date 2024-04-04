de32253aabf47f1b504fde0fb1613698955f7268
William Navarre <navarre@dimins.com>
2024-04-03 22:54:53 -0400

    Update my_task for github.

--- script/my_task.py
+++ script/my_task.py
@@ -5,14 +5,10 @@ from git_task import *
 
 os.chdir(os.getenv("HOME"))
 
-if not os.path.exists("test"):
-    subprocess.run([ "git", "clone", "/root/test.git" ])
-else:
-    os.chdir("test")
-    subprocess.run([ "git", "pull", "origin", "main", "--ff-only" ])
-    subprocess.run([ "git", "reset", "--hard", "origin/main" ])
-    os.chdir(os.getenv("HOME"))
+os.chdir("/root/medford-meeting-podcast")
+subprocess.run([ "git", "pull", "origin", "main", "--ff-only" ])
+subprocess.run([ "git", "reset", "--hard", "origin/main" ])
 
-my_task_project = TaskProject("./test", "tasks.dat")
+my_task_project = TaskProject("/root/medford-meeting-podcast", "tasks.dat")
 
 __all__ = [ "my_task_project" ]
